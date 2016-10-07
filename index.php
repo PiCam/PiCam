5713f43960f0dd733d00859bfd4b65455a4803fa
PiCam <picamproject@gmail.com>
2016-10-07 12:24:52 -0400

    Fixed old index.html location

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="refresh" content="0; URL='http://192.168.0.236/vidstream.php'" />
+		<title>Password Page</title>
+	</head>
+	<body>
+	</body>
+	
+</html>
